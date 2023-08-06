3d16ca870d0ac730645ea753d7bf0f2a34c5578a
dominic <rednman@gmail.com>
2023-08-06 19:29:22 +0900

    공공데이터 연습

---
+++ tests/commercialBusinessAPI.py
@@ -0,0 +1,25 @@
+import json
+import requests
+import xmltodict
+
+
+
+url = "http://data.ex.co.kr/openapi/toll/bhoinstIntoTollList"
+queryParams = {
+  'key' : 'test',
+  'type' : 'xml',
+  'umOfRows' : '10',
+  'arrvTolofCd' : '101',
+  'dprtrTolofCd' : '102',
+  'hoinstCd' : '00',
+  'pageNo' : '1',
+  'numOfRows' : '10'
+}
+
+repones = requests.get(url, params=queryParams)
+
+print(repones.status_code)
+data = repones.text
+diction = xmltodict.parse(data)
+jsonString = json.dumps(diction, indent=4)
+print(jsonString)
